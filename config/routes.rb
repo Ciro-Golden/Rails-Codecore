b8eedca3739b6cfff0df317c3b8a74074bd7d256
ciro golden <cgoldennlo@gmail.com>
2019-01-21 02:00:03 -0800

    Signup Users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   resources :posts
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new] #want all the routes except the new user Route. 
 end
